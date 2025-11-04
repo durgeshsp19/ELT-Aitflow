361653498fd72f63d76b64d61b2cfd3df43164f8
durgeshsp19 <143993381+durgeshsp19@users.noreply.github.com>
2025-11-04 06:35:46 +0530

    Refactor DAG for loading and transforming health data

--- 02_transformation.py
+++ 02_transformation.py
@@ -14,6 +14,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
 }
+
 # Project configuration
 project_id = 'tt-dev-02'
 dataset_id = 'staging_dataset'
@@ -21,6 +22,7 @@ transform_dataset_id = 'transform_dataset'
 
 source_table = f'{project_id}.{dataset_id}.global_data'
 countries = ['USA', 'India', 'Germany', 'Japan', 'France', 'Canada', 'Italy']
+
 with DAG(
     dag_id='load_and_transform',
     default_args=default_args,
@@ -34,24 +36,24 @@ with DAG(
     # Task 1: Check if the file exists in GCS
     check_file_exists = GCSObjectExistenceSensor(
         task_id='check_file_exists',
-        bucket='bkt-src-global-data',  # Replace with your bucket name
-        object='global_health_data.csv',  # Replace with the file path in the bucket
-        timeout=300,  # Maximum wait time in seconds
-        poke_interval=30,  # Time interval in seconds to check again
+        bucket='bkt-src-global-data',
+        object='global_health_data.csv',
+        timeout=300,
+        poke_interval=30,
         mode='poke',
     )
 
     # Task 2: Load CSV from GCS to BigQuery
     load_csv_to_bigquery = GCSToBigQueryOperator(
         task_id='load_csv_to_bq',
-        bucket='bkt-src-global-data',  # Replace with your bucket name
-        source_objects=['global_health_data.csv'],  # Path to your file in the bucket
+        bucket='bkt-src-global-data',
+        source_objects=['global_health_data.csv'],
         destination_project_dataset_table=source_table,
         source_format='CSV',
         allow_jagged_rows=True,
         ignore_unknown_values=True,
-        write_disposition='WRITE_TRUNCATE',  # Options: WRITE_TRUNCATE, WRITE_APPEND, WRITE_EMPTY
-        skip_leading_rows=1,  # Skip header row
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
         field_delimiter=',',
         autodetect=True,
     )
@@ -68,7 +70,7 @@ with DAG(
                         FROM `{source_table}`
                         WHERE country = '{country}'
                     """,
-                    "useLegacySql": False,  # Use standard SQL syntax
+                    "useLegacySql": False,
                 }
             },
         ).set_upstream(load_csv_to_bigquery)
